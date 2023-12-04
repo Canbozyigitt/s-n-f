0865fac056f54e0234a28108009456ab36bf2dcb
CANO <umutcanbozyigit34012@gmail.com>
2023-12-04 18:00:54 +0300

    sınıflar

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+class Person:
+    def __init__(self,name):
+        self.name=name
+
+    def talk(self):
+        print(f"hi {self.name}")
+
+
+
+can=Person("cano")
+can.talk()          
+'''
+print("hello")
